895b4a0ae50f6c9278c87bd7e3c053d542a45ce0
CHANDANA HH <139252019+CHANDANA-HH@users.noreply.github.com>
2026-02-24 13:03:44 +0530

    Create argLength.js

---
+++ argLength.js
@@ -0,0 +1,12 @@
+
+
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+var argumentsLength = function(...args) {
+
+    let arr=[...args]
+    return arr.length;
+    
+};
+
+
