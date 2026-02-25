ba61616e3401340d41e944f5dd3049f2adf5e8db
CHANDANA HH <139252019+CHANDANA-HH@users.noreply.github.com>
2026-02-25 11:50:44 +0530

    Add function to sum two resolved promises
    Implement a function to add results of two promises.

---
+++ addPromise.js
@@ -0,0 +1,8 @@
+// Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two number
+
+var addTwoPromises = async function(promise1, promise2) {
+
+   let ans1 = await promise1;
+   let ans2 = await promise2;
+
+   return ans1+ans2;
